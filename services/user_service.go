37b81e3c3a6a43f18e7479090cca56861ccc59b3
sojung.kim <thwjd2717@gmail.com>
2024-11-08 22:49:35 +0900

    [#3] chore: error 문구 수정

--- services/user_service.go
+++ services/user_service.go
@@ -27,7 +27,7 @@ func (s *authService) SignUp(user *models.User) error {
 	// TODO: 이메일 중복시 클라이언트단에 알림
 	_, err := s.userRepository.GetUserByEmail(user.Email)
 	if err == nil {
-		return errors.New("email already exists")
+		return errors.New("이미 등록된 이메일입니다")
 	}
 
 	// 비밀번호 해싱
@@ -45,12 +45,12 @@ func (s *authService) SignUp(user *models.User) error {
 func (s *authService) Login(email, password string) (string, error) {
 	user, err := s.userRepository.GetUserByEmail(email)
 	if err != nil {
-		return "", errors.New("user not found")
+		return "", errors.New("가입되지 않은 메일입니다")
 	}
 
 	// 비밀번호 검증
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
-		return "", errors.New("incorrect password")
+		return "", errors.New("비밀번호가 일치하지 않습니다")
 	}
 
 	// JWT 토큰 생성
