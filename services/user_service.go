84ba2ff74d06270415338ff1ef3b3e7dcaa1ac1b
sojung.kim <thwjd2717@gmail.com>
2024-11-08 22:48:13 +0900

    [#3] feat: 회원가입, 로그인 서비스 로직 구현

---
+++ services/user_service.go
@@ -0,0 +1,62 @@
+package services
+
+import (
+	"errors"
+	"github.com/zeze1004/image-hub-platform/models"
+	"github.com/zeze1004/image-hub-platform/repositories"
+	"github.com/zeze1004/image-hub-platform/utils"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService interface {
+	SignUp(user *models.User) error
+	Login(email, password string) (string, error) // TODO: JWT 토큰 리턴 수정
+}
+
+// authService 구조체
+type authService struct {
+	userRepository repositories.UserRepository
+}
+
+func NewAuthService(userRepo repositories.UserRepository) AuthService {
+	return &authService{userRepository: userRepo}
+}
+
+// SignUp - 새로운 사용자 등록
+func (s *authService) SignUp(user *models.User) error {
+	// TODO: 이메일 중복시 클라이언트단에 알림
+	_, err := s.userRepository.GetUserByEmail(user.Email)
+	if err == nil {
+		return errors.New("email already exists")
+	}
+
+	// 비밀번호 해싱
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+
+	// 사용자 저장
+	return s.userRepository.CreateUser(user)
+}
+
+// Login - 이메일과 비밀번호로 로그인 처리
+func (s *authService) Login(email, password string) (string, error) {
+	user, err := s.userRepository.GetUserByEmail(email)
+	if err != nil {
+		return "", errors.New("user not found")
+	}
+
+	// 비밀번호 검증
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		return "", errors.New("incorrect password")
+	}
+
+	// JWT 토큰 생성
+	token, err := utils.GenerateToken(user.ID, user.Role)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
