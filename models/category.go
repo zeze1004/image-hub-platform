0851e2986f4764d307880c9b754d77e58d04ec43
sojung.kim <thwjd2717@gmail.com>
2024-11-10 20:16:42 +0900

    [#23] chore: 외부에서 참조할 수 있게 대문자로 변경

--- models/category.go
+++ models/category.go
@@ -5,5 +5,5 @@ import "gorm.io/gorm"
 type Category struct {
 	gorm.Model
 	ID   uint   `gorm:"primaryKey;autoIncrement"`
-	name string `gorm:"unique;not null"`
+	Name string `gorm:"unique;not null"`
 }
